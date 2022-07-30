da051a668b674b6fa3ad81ce7540706ee31bbc49
BijanEisapour <bijanprogrammer@gmail.com>
2022-07-30 15:21:19 +0430

    [ADD] cachedIsLoggedIn and cachedUserId

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,26 +9,44 @@ import {IdObject} from '../models/api/id-object.model';
     providedIn: 'root',
 })
 export class AuthService {
+    public cachedIsLoggedIn: boolean | null = null;
+    public cachedUserId: number | null = null;
+
     public constructor(private apiService: ApiService) {}
 
-    public async login(user: User): Promise<boolean> {
-        const data = await this.apiService.postRequest<TokenObject>({url: API_USER_LOGIN, body: user});
-
-        if (data?.token) {
-            localStorage.setItem('token', data?.token);
-        }
-
-        return !!data;
+    private static get token(): string {
+        return localStorage.getItem('token') || '';
     }
 
     public async isLoggedIn(): Promise<boolean> {
-        const token = localStorage.getItem('token') || '';
+        if (this.cachedIsLoggedIn !== null) return this.cachedIsLoggedIn;
+        return await this.auth();
+    }
 
-        const data = await this.apiService.postRequest<IdObject>({
+    public async login(user: User): Promise<boolean> {
+        const response = await this.apiService.postRequest<TokenObject>({url: API_USER_LOGIN, body: user});
+        if (!response) return false;
+
+        await this.saveCache(response.token, true, response.id);
+        return !!response;
+    }
+
+    private async auth(): Promise<boolean> {
+        const response = await this.apiService.postRequest<IdObject>({
             url: API_USER_AUTH,
-            body: {token},
+            body: {token: AuthService.token},
             showError: false,
         });
-        return !!data;
+
+        await this.saveCache(AuthService.token, !!response, response?.id ?? null);
+        return !!response;
+    }
+
+    private async saveCache(token: string | null, isLoggedIn: boolean, userId: number | null): Promise<void> {
+        if (!!token) localStorage.setItem('token', token);
+        else localStorage.removeItem('token');
+
+        this.cachedIsLoggedIn = isLoggedIn;
+        this.cachedUserId = userId;
     }
 }
