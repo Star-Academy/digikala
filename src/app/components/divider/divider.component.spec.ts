d9b5c95127be198f38f61c192a740b913e964472
BijanEisapour <bijanprogrammer@gmail.com>
2022-07-31 11:25:04 +0430

    [TEST][ADD] divider

--- src/app/components/divider/divider.component.spec.ts
+++ src/app/components/divider/divider.component.spec.ts
@@ -1,1 +1,50 @@
-describe('DividerComponent', () => {});
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {DividerComponent} from './divider.component';
+
+describe('DividerComponent', () => {
+    let fixture: ComponentFixture<DividerComponent>;
+    let component: DividerComponent;
+    let host: HTMLElement;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [DividerComponent],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DividerComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        host = fixture.nativeElement as HTMLElement;
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should render line - default inputs', () => {
+        testVariation();
+    });
+
+    const variations: Array<'vertical' | 'horizontal'> = ['vertical', 'horizontal'];
+    for (const variation of variations) {
+        it(`should render line - ${variation}`, () => {
+            testVariation(variation);
+        });
+    }
+
+    // [SECTION] Utility Functions
+
+    const testVariation = (variation?: 'vertical' | 'horizontal'): void => {
+        if (!!variation) {
+            component.variation = variation;
+            fixture.detectChanges();
+        }
+
+        const line = host.querySelector('i');
+
+        expect(line).toBeTruthy();
+        expect(line?.className).toEqual(variation || 'vertical');
+    };
+});
