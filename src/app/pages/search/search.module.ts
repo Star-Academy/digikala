61c934abd8ca623953755970251e0875eb79c7f4
BijanEisapour <bijanprogrammer@gmail.com>
2022-08-03 11:59:42 +0430

    [ADD] search page - filters - switch

--- src/app/pages/search/search.module.ts
+++ src/app/pages/search/search.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SearchComponent} from './search.component';
 import {BreadcrumbsModule} from '../../components/breadcrumbs/breadcrumbs.module';
-import { FiltersComponent } from './components/filters/filters.component';
-import { GamesComponent } from './components/games/games.component';
+import {FiltersComponent} from './components/filters/filters.component';
+import {GamesComponent} from './components/games/games.component';
+import {SwitchComponent} from './components/switch/switch.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-    declarations: [SearchComponent, FiltersComponent, GamesComponent],
-    imports: [CommonModule, BreadcrumbsModule],
+    declarations: [SearchComponent, FiltersComponent, GamesComponent, SwitchComponent],
+    imports: [CommonModule, BreadcrumbsModule, FormsModule],
 })
 export class SearchModule {}
